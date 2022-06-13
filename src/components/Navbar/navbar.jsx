a4cca7d01a2f5b5b5e2e30c9bd3f35d276075a1f
muhammad2503 <abdullaevmuhammadyusuf2@gmail.com>
2022-06-13 11:45:23 +0500

    'yusuf'

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -12,32 +12,11 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import { styled } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import AppleIcon from '@mui/icons-material/Apple';
-import GoogleIcon from '@mui/icons-material/Google';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import './style.css'
-import { color } from '@mui/system';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-const pages = ['Home', 'Shop', 'Sale','Features','Blog','About','Contact'];
-// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-const ResponsiveAppBar = () => {
+const Nav = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -57,50 +36,10 @@ const ResponsiveAppBar = () => {
   };
 
   return (
-    <AppBar position="static" style={{backgroundColor:'white', color:'black'}}>
-       <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'none', lg:'flex'}, backgroundColor:'#F5F5F5' }}>
-       <Container maxWidth>
-<Grid container spacing={2}>
- <Grid item xs={12}>
-     <>
-       <div style={{float:'left',marginTop:'0.5%'}}>
-         <Tooltip title='Facebook'>
-         <FacebookIcon className='A'/>
-         </Tooltip>
-         <Tooltip title='Twitter'>
-         <TwitterIcon className='A'/>
-         </Tooltip>
-         <Tooltip title='Apple'>
-         <AppleIcon className='A'/>
-         </Tooltip>
-         <Tooltip title='Google'>
-         <GoogleIcon className='A'/>
-         </Tooltip>
-         <Tooltip title='Instagram'>
-         <InstagramIcon className='A'/>
-         </Tooltip>
-         </div>
-        <p style={{textAlign:'center'}}>Free shipping for standard order over $100</p>
-        <div style={{float:'right',display:'flex',marginTop:'-2.5%'}}>
-       <p>fashe@example.com</p>
-  <select style={{border:'none',color:'GrayText',backgroundColor:'#F5F5F5'}} className='B'>
-          <option value="2" className='B'>USD</option>
-          <option value="3">EURO</option>
-         <option value="4">SUM</option>
-        <option value="5">GBP</option>
-        <option value="6">LIRA</option>
-         <option value="8">TANGE</option>
-      </select>
-   </div>
-    </>
-        
-  </Grid>
- </Grid>
- </Container>
- </Box>
- <Container maxWidth>
+    <AppBar position="static">
+      <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -116,7 +55,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            <h1 style={{fontFamily:'unset',fontWeight:'unset'}}>Fashe.</h1>
+            LOGO
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -155,6 +94,7 @@ const ResponsiveAppBar = () => {
               ))}
             </Menu>
           </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -171,14 +111,14 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            <h1 style={{fontSize:'100%'}}>Fashe.</h1>
+            LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' ,textAlign:'center',justifyContent:'center'} }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
-              <Button className='A'
+              <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'black', display: 'block'}}
+                sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 {page}
               </Button>
@@ -186,37 +126,32 @@ const ResponsiveAppBar = () => {
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Account">
+            <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
-                <AccountCircleIcon className='A' style={{fontSize:'100%',":hover":{backgroundColor:'red'}}}/>
-        
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Shopping">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
-                <ShoppingBagIcon className='A' style={{marginLeft:'5%'}}/>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' ,width:'100%' ,xs:'100%'}}
+              sx={{ mt: '45px' }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
                 vertical: 'top',
-                horizontal: 'end',
+                horizontal: 'right',
               }}
               keepMounted
               transformOrigin={{
                 vertical: 'top',
-                horizontal: 'end',
+                horizontal: 'right',
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
-              style={{width:'100%'}}
             >
-             
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
             </Menu>
           </Box>
         </Toolbar>
@@ -224,4 +159,4 @@ const ResponsiveAppBar = () => {
     </AppBar>
   );
 };
-export default ResponsiveAppBar;
+export default ApBar;
--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,18 +1,23 @@
 import React from "react";
-import Image_list from "../../components/image_list";
+// import Image_list from "../../components/image_list";
 import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
-import LookBook from "../../components/LookBook";
-import Blog from "../../components/blog";
+import Carousel from "../../components/Carusel";
+// import ApBar from "../../components/Navbar/navbar";
+
+// import Footer from "../../components/Footer";
+// import LookBook from "../../components/LookBook";
+// import Blog from "../../components/blog";
 
 export default () => {
 	return (
 		<>
 			<Navbar />
-			<Image_list />
+			<Carousel/>
+			{/* <ApBar/> */}
+			{/* <Image_list />
 			<LookBook />
 			<Blog />
-			<Footer />
+			<Footer /> */}
 		</>
 	);
 };
